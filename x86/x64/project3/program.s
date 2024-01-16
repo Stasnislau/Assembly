575fd7366320b3c5b7b64216788e52a4bdc45b74
stasnislau <stanislaurv@gmail.com>
2024-01-16 19:46:03 +0100

    updated according to calling convention

--- x86/x64/project3/program.s
+++ x86/x64/project3/program.s
@@ -1,91 +1,86 @@
-    section .text 
-    global  emphasize8
+section .text 
+global  emphasize8
+
 emphasize8: ; 64-bit version
     push r11
-    push r12
+    push r12 
     push r13
     push r14
     push r15
     mov r10, rdi ; image pointer
-    mov r11, rsi ; image width
-    mov r12, rdx ; image height
-    mov r13, rcx ; image stride
-    sub r13, r11 ; stride - width, OFFSET to next row
-    xor r14, r14 ; lightest shade, initially darkest possible
-    xor r15, r15 ; darkest shade, initially lightest possible
-    mov r14, 0
-    mov r15, 255
+    mov r11, rsi ; image width, saved as argument
+    mov r12d, edx ; image height, saved as argument
+    mov r13d, ecx ; image stride, saved as argument
 
-find_traverse:
-    test r11, r11 
-    jz detect_end_of_row
-    xor rax, rax
-    mov al, [r10]
-    cmp rax, r15
-    jb set_black
-    cmp rax, r14
-    ja set_white
-    inc r10
-    dec r11
-    jmp find_traverse
+    mov     eax, r11d ; image width
+    mov     ecx, r12d ; image height
+    mov     edx, r13d ; image stride
+    sub     edx, eax ; stride - width, offset to next row
+    xor     bx, bx ;  upper part is darkest, lower part is lightest
+    mov     bh, 0xff ; darkest shade, initially lightest possible
+    mov     bl, 0x00 ; lightest shade, initially darkest possible
+    xor     r9, r9
+    movzx   r9d, dl ; image stride
 
-set_black:
-    mov r15, rax
-    jmp find_traverse
+find_traverse: 
+    mov   dl, [r10] ; load pixel value
+    cmp   dl, bh 
+    jae   skip_to_white
+    mov   bh, dh
 
-set_white:
-    mov r14, rax
-    jmp find_traverse
+skip_to_white:
+    cmp   dl, bl
+    jbe   skip
+    mov   bl, dl
+
+skip: 
+    inc   r10
+    dec   eax
+    jnz   find_traverse
 
 detect_end_of_row:
-    test r12, r12
-    jz finish_traverse
-    xor rax, rax
-    dec r12
-    test r12, r12
-    jz finish_traverse
-    add r10, r13 ; image pointer + offset to next row
-    mov r11, rsi
-    jmp find_traverse
+    xor   dl, dl
+    add   r10, r9 ; image pointer + offset to next row
+    mov   eax, r11d ; image width
+    dec   ecx
+    jnz   find_traverse
+
 
 finish_traverse:
-    mov r10, rdi ; image pointer
-    mov r11, rsi ; image width
-    mov r12, rdx ; image height
+    mov   r10, rdi ; image pointer
+    mov   r14d, r11d ; image width
+    mov   ecx, r12d ; image height
 
-change_traverse:
-    test r11, r11
-    jz detect_end_of_row_change
-    xor rax, rax
-    mov al, [r10]
-    sub rax, r15 ; al = pixel value - darkest shade
-    mov rdx, 255
-    mul rdx ; eax = (pixel value - darkest shade) * 255
-    xor rdx, rdx
-    mov r8, r14 ;
-    sub r8, r15 ;
-    div r8 ; eax = (pixel value - darkest shade) * 255 / (lightest shade - darkest shade)
-    mov [r10], al
-    inc r10
-    dec r11
-    jmp change_traverse
+
+change_traverse: 
+    xor   eax, eax
+    mov   al, [r10] 
+    sub   al, bh ; al = pixel value - darkest shade
+    mov   edx, 255 ; edx = 255
+    mul   edx ; eax = (pixel value - darkest shade) * 255
+    xor   edx, edx
+    mov   dl, bl
+    sub   dl, bh 
+    mov   r15d, edx ; r15d = lightest shade - darkest shade
+    xor   edx, edx
+    div   r15d ; eax = (pixel value - darkest shade) * 255 / (lightest shade - darkest shade) 
+    mov   [r10], al ; store new pixel value
+    inc   r10
+    dec   r14d 
+    jnz   change_traverse
 
 
 detect_end_of_row_change:
-    test r12, r12
-    jz return
-    dec r12
-    test r12, r12
-    jz return
-    add r10, r13 ; image pointer + offset to next row
-    mov r11, rsi ; image width
-    jmp change_traverse
+    mov   r14d, r11d ; width
+    add   r10, r9 ; image pointer + offset to next row
+    dec   ecx 
+    jnz   change_traverse
 
 return:
+    mov rax, rdi
     pop r15
     pop r14
     pop r13
     pop r12
     pop r11
-    mov    rax, rdi
     ret
